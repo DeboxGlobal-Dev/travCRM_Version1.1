4cd8c8705107367f264a83199bf6c2fb01fba0e4
Satendra Gurjar <78408436+satendragurjar844@users.noreply.github.com>
2023-11-24 15:20:27 +0530

    changes

--- Code/Services/routes/api.php
+++ Code/Services/routes/api.php
@@ -3,22 +3,22 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Others\Master\ExampleController;
-use App\Http\Controllers\Others\Master\CityMasterController;
-use App\Http\Controllers\Others\Master\StateMasterController;
-use App\Http\Controllers\Others\Master\CountryMasterController;
-use App\Http\Controllers\Others\Master\LanguageMasterController;
-use App\Http\Controllers\Others\Master\DestinationMasterController;
 use App\Http\Controllers\Others\Master\BusinessTypeMasterController;
+use App\Http\Controllers\Others\Master\CountryMasterController;
+use App\Http\Controllers\Others\Master\StateMasterController;
+use App\Http\Controllers\Others\Master\CityMasterController;
+use App\Http\Controllers\Others\Master\DestinationMasterController;
+use App\Http\Controllers\Others\Master\LanguageMasterController;
 
 Route::post('/countrylist',[CountryMasterController::class,'index']);
 Route::post('/addupdatecountry',[CountryMasterController::class,'store']);
 
-Route::post('/statelist',[StateMasterController::class,'index']);
-Route::post('/addupdatestate',[StateMasterController::class,'store']);
-Route::post('/deletestate',[StateMasterController::class,'destroy']);
+Route::post('statelist',[StateMasterController::class,'index']);
+Route::post('addupdatestate',[StateMasterController::class,'store']);
+Route::post('deletestate',[StateMasterController::class,'destroy']);
 
 Route::post('/citylist',[CityMasterController::class,'index']);
-Route::post('/addupdatecity',[CityMasterController::class,'store']);
+Route::post('/addupdatecity',[CityMasterController::class,'save']);
 
 Route::post('/destinationlist',[DestinationMasterController::class,'index']);
 Route::post('/addupdatedestination',[DestinationMasterController::class,'store']);
@@ -30,7 +30,7 @@ Route::post('/languagelist',[LanguageMasterController::class,'index']);
 Route::post('/addupdatelanguage',[LanguageMasterController::class,'store']);
 
 Route::post('/querylist',[QueryMasterController::class,'index']);
-Route::post('/addupdatequery',[QueryMasterController::class,'store']);
+Route::post('/addupdatequery',[QueryMasterController::class,'save']);
 
 Route::post('/users-api',[ExampleController::class,'apidata']);
 
