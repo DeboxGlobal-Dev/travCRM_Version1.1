8e99beff1de348e96e8897b055595ccf7e030844
prasangNegi <prasang.negi@deboxglobal.com>
2024-01-04 12:38:39 +0530

    search hotel rates

--- Code/Services/app/Helper/helper.php
+++ Code/Services/app/Helper/helper.php
@@ -1,6 +1,8 @@
 
 <?php
 
+use Carbon\Carbon;
+
 function call_logger($errorlog){
     $isActive = true;
     if($isActive){
@@ -35,6 +37,57 @@ function getColumnValue($tableName,$where,$val,$columnName){
     if($value->isNotEmpty()){
         return $value[0]->$columnName;
     }
+}
+
+
+function getCodes($from,$to,$destination){
+
+    $firstmonth = Carbon::parse($from)->format('n');
+    $lastmonth = Carbon::parse($to)->format('n');
+    $firstyear = Carbon::parse($from)->year;
+    $lastyear = Carbon::parse($to)->year;
+    $arr = array();
+
+    if($firstyear == $lastyear){
+
+        for($i = $firstmonth;$i <= $lastmonth;$i++){
+            array_push($arr,"V1^V31^M".$i."^D".$destination."^Y".$firstyear);
+        }
+
+    }else{
+
+        for($j = $firstyear;$j <= $lastyear;$j++){
+
+            if($j == $firstyear){
+
+                for($z = $firstmonth;$z <= 12;$z++){
+                    array_push($arr,"V1^V31^M".$z."^D".$destination."^Y".$j);
+                }
+
+            }
+            if($j == $lastyear){
+
+                for($z = 1;$z <= $lastmonth;$z++){
+                    array_push($arr,"V1^V31^M".$z."^D".$destination."^Y".$j);
+                }
+
+            }
+            if($j != $firstyear && $j != $lastyear){
+
+                for($z = 1;$z <= 12;$z++){
+                    array_push($arr,"V1^V31^M".$z."^D".$destination."^Y".$j);
+                }
+
+            }
+
+
+        }
+
+    }
+
+return $arr;
+
+
 }
 
 
--- Code/Services/app/Models/Hotel/Master/SearchHotelRate.php
+++ Code/Services/app/Models/Hotel/Master/SearchHotelRate.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class SearchHotelRate extends Model
 {
     use HasFactory;
+    protected $table = _SEARCH_HOTEL_RATE_;
+    protected $primarykey = 'id';
+    protected $fillable = [
+        'RateCode', 'JsonResult'
+    ];
+    public $timestamps = false;
 }
