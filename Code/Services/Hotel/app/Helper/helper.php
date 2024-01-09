b11bb5c023ce974db7518231f693ef7c56f99a6d
Satendra Gurjar <78408436+satendragurjar844@users.noreply.github.com>
2024-01-09 13:33:51 +0530

    get code function

--- Code/Services/Hotel/app/Helper/helper.php
+++ Code/Services/Hotel/app/Helper/helper.php
@@ -11,16 +11,16 @@ function call_logger($errorlog){
         {
             Storage::put($newfile, '');
         }
-    
+
         $logfile=fopen(Storage::path($newfile),'a');
-        
+
         $ip = \Request::ip();
         date_default_timezone_set('Asia/Kolkata');
         $time = date('d-m-Y h:i:s A',time());
         $contents = "$ip\t$time\t$errorlog\r";
         fwrite($logfile,$contents);
-        
-    }	
+
+    }
 }
 
 function getName($tableName,$id){
@@ -28,7 +28,7 @@ function getName($tableName,$id){
     if($name->isNotEmpty()){
         return $name[0]->Name;
     }
-    
+
 }
 
 function getColumnValue($tableName,$where,$val,$columnName){
@@ -36,6 +36,56 @@ function getColumnValue($tableName,$where,$val,$columnName){
     if($value->isNotEmpty()){
         return $value[0]->$columnName;
     }
+}
+
+function getCodes($from,$to,$destination){
+
+    $firstmonth = Carbon::parse($from)->format('n');
+    $lastmonth = Carbon::parse($to)->format('n');
+    $firstyear = Carbon::parse($from)->year;
+    $lastyear = Carbon::parse($to)->year;
+    $arr = array();
+
+    if($firstyear == $lastyear){
+
+        for($i = $firstmonth;$i <= $lastmonth;$i++){
+            array_push($arr,"V1^V31^M".$i."^D".$destination."^Y".$firstyear);
+        }
+
+    }else{
+
+        for($j = $firstyear;$j <= $lastyear;$j++){
+
+            if($j == $firstyear){
+
+                for($z = $firstmonth;$z <= 12;$z++){
+                    array_push($arr,"V1^V31^M".$z."^D".$destination."^Y".$j);
+                }
+
+            }
+            if($j == $lastyear){
+
+                for($z = 1;$z <= $lastmonth;$z++){
+                    array_push($arr,"V1^V31^M".$z."^D".$destination."^Y".$j);
+                }
+
+            }
+            if($j != $firstyear && $j != $lastyear){
+
+                for($z = 1;$z <= 12;$z++){
+                    array_push($arr,"V1^V31^M".$z."^D".$destination."^Y".$j);
+                }
+
+            }
+
+
+        }
+
+    }
+
+return $arr;
+
+
 }
 
 
