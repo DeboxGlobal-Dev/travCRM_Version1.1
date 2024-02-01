4f2b3d71360ca22673d212678aa45a3ee5de758b
Satendra Gurjar <78408436+satendragurjar844@users.noreply.github.com>
2024-02-01 14:23:46 +0530

    query builder

---
+++ Code/Querybuilder/app/Helper/dbtable.php
@@ -0,0 +1,13 @@
+<?php
+////////////////Define all table name here/////////////////////
+
+/*-------FOR CUSTOM DB SELECTION  PGSQL OR MYSQL ------*/
+define("_DB_", "pgsql");
+/*-----------------------------------------------------*/
+
+///////////////////////FOR QUERY BUILDER ONLY/////////////////////////////
+define("_QUERY_MASTER_", "others.query_master");
+
+///////////////////////END HERE/////////////////////////////?
+
+?>
