521a7db8fbce2625b2e7c0c588e71c1b4c006214
Satendra Gurjar <78408436+satendragurjar844@users.noreply.github.com>
2024-02-15 17:06:22 +0530

    condition change

--- Code/Services/Others/app/Http/Controllers/Master/CountryMasterController.php
+++ Code/Services/Others/app/Http/Controllers/Master/CountryMasterController.php
@@ -9,46 +9,37 @@ use App\Models\Master\CountryMaster;
 
 class CountryMasterController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         $Search = $request->input('Search');
         $Status = $request->input('Status');
-        $SetDefault = $request->input('SetDefault');
 
         $posts = CountryMaster::when($Search, function ($query) use ($Search) {
-            return $query->where('Name', 'like', '%' . $Search . '%')
-                   ->orwhere('ShortName', 'like', '%' . $Search . '%');
+            return $query->where('Name', 'ilike', '%' . $Search . '%')
+                ->orwhere('ShortName', 'ilike', '%' . $Search . '%');
         })->when($Status, function ($query) use ($Status) {
-             return $query->where('Status',$Status);
-        })->when($SetDefault, function ($query) use ($SetDefault) {
-            return $query->where('SetDefault',$SetDefault);
-       })->select('*')->orderBy('Name')->get('*');
+            return $query->where('Status', $Status);
+        })->select('*')->orderBy('Name')->get('*');
 
         if ($posts->isNotEmpty()) {
             $arrayDataRows = [];
-            foreach ($posts as $post){
-                if($Status == 0 && $SetDefault == 0){
-                    $Status = 'Active';
-                    $SetDefault = 'False';
-               }elseif ($Status == 1 && $SetDefault == 1) {
-                    $Status = 'InActive';
-                    $SetDefault = 'True';
-               }
+            foreach ($posts as $post) {
+
                 $arrayDataRows[] = [
                     "Id" => $post->id,
                     "Name" => $post->Name,
                     "ShortName" => $post->ShortName,
-                    "SetDefault" => $SetDefault,
-                    "Status" => $Status,
+                    "SetDefault" => ($post->SetDefault == 1) ? 'Yes' : 'No',
+                    "Status" => ($post->Status == 1) ? 'Active' : 'Inactive',
                     "AddedBy" => $post->AddedBy,
                     "UpdatedBy" => $post->UpdatedBy,
                     "Created_at" => $post->created_at,
                     "Updated_at" => $post->updated_at
                 ];
             }
-    
+
             return response()->json([
                 'Status' => 0,
-                'message' => '',
                 'TotalRecord' => $posts->count('id'),
                 'DataList' => $arrayDataRows
             ]);
@@ -63,51 +54,50 @@ class CountryMasterController extends Controller
 
     public function store(Request $request)
     {
-        try{
+        try {
             $id = $request->input('id');
-            if($id == '') {
+            if ($id == '') {
 
-                $businessvalidation =array(
-                    'Name' => 'required|unique:'._DB_.'.'._COUNTRY_MASTER_.',Name',
+                $businessvalidation = array(
+                    'Name' => 'required|unique:' . _DB_ . '.' . _COUNTRY_MASTER_ . ',Name',
                     'ShortName' => 'required'
                 );
 
                 $validatordata = validator::make($request->all(), $businessvalidation);
 
-                if($validatordata->fails()){
+                if ($validatordata->fails()) {
                     return $validatordata->errors();
-                }else{
-                 $savedata = CountryMaster::create([
-                    'Name' => $request->Name,
-                    'ShortName' => $request->ShortName,
-                    'SetDefault' => $request->SetDefault,
-                    'Status' => $request->Status,
-                    'AddedBy' => $request->AddedBy,
-                    'created_at' => now(),
-                ]);
-
-                if ($savedata) {
-                    return response()->json(['Status' => 0, 'Message' => 'Data added successfully!']);
                 } else {
-                    return response()->json(['Status' => 1, 'Message' =>'Failed to add data.'], 500);
-                }
-              }
+                    $savedata = CountryMaster::create([
+                        'Name' => $request->Name,
+                        'ShortName' => $request->ShortName,
+                        'SetDefault' => $request->SetDefault,
+                        'Status' => $request->Status,
+                        'AddedBy' => $request->AddedBy,
+                        'created_at' => now(),
+                    ]);
 
-            }else{
+                    if ($savedata) {
+                        return response()->json(['Status' => 0, 'Message' => 'Data added successfully!']);
+                    } else {
+                        return response()->json(['Status' => 1, 'Message' => 'Failed to add data.'], 500);
+                    }
+                }
+            } else {
 
                 $id = $request->input('id');
                 $edit = CountryMaster::find($id);
 
-                $businessvalidation =array(
-                    'Name' => 'required|unique:'._DB_.'.'._COUNTRY_MASTER_.',Name',
+                $businessvalidation = array(
+                    'Name' => 'required|unique:' . _DB_ . '.' . _COUNTRY_MASTER_ . ',Name',
                     'ShortName' => 'required'
                 );
 
                 $validatordata = validator::make($request->all(), $businessvalidation);
 
-                if($validatordata->fails()){
-                 return $validatordata->errors();
-                }else{
+                if ($validatordata->fails()) {
+                    return $validatordata->errors();
+                } else {
                     if ($edit) {
                         $edit->Name = $request->input('Name');
                         $edit->ShortName = $request->input('ShortName');
@@ -123,8 +113,8 @@ class CountryMasterController extends Controller
                     }
                 }
             }
-        }catch (\Exception $e){
-            call_logger("Exception Error  ===>  ". $e->getMessage());
+        } catch (\Exception $e) {
+            call_logger("Exception Error  ===>  " . $e->getMessage());
             return response()->json(['Status' => -1, 'Message' => 'Exception Error Found']);
         }
     }
