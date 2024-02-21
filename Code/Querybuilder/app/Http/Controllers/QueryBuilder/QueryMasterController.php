0021f415fb92e17a78e7170e72f29bb1ec09a5ae
Satendra Gurjar <78408436+satendragurjar844@users.noreply.github.com>
2024-02-21 11:53:22 +0530

    uodate in querymaster

--- Code/Querybuilder/app/Http/Controllers/QueryBuilder/QueryMasterController.php
+++ Code/Querybuilder/app/Http/Controllers/QueryBuilder/QueryMasterController.php
@@ -30,30 +30,15 @@ class QueryMasterController extends Controller
                 $arrayDataRows[] = [
                     "Id" => $post->id,
                     "QueryId" => $post->QueryId,
-                    "FDCode" => $post->FDCode,
-                    "PackageCode" => $post->PackageCode,
-                    "PackageName" => $post->PackageName,
                     "ClientType" => $post->ClientType,
-                    "AgentId" => $post->AgentId,
                     "LeadPax" => $post->LeadPax,
                     "Subject" => $post->Subject,
-                    "AddEmail" => $post->AddEmail,
-                    "AdditionalInfo" => $post->AdditionalInfo,
                     "QueryType" => $post->QueryType,
-                    "ValueAddedServices" => $post->ValueAddedServices,
-                    "TravelInfo" => $post->TravelInfo,
-                    "PaxType" => $post->PaxType,
-                    "TravelDate" => $post->TravelDate,
-                    "PaxInfo" => $post->PaxInfo,
-                    "RoomInfo" => $post->RoomInfo,
                     "Priority" => $post->Priority,
                     "TAT" => $post->TAT,
-                    "TourType" => $post->TourType,
                     "LeadSource" => $post->LeadSource,
-                    "LeadRefrenceId" => $post->LeadRefrenceId,
-                    "HotelPrefrence" => $post->HotelPrefrence,
-                    "HotelType" => $post->HotelType,
-                    "MealPlan" => $post->MealPlan,
+                    "FromDate" => $post->FromDate,
+                    "ToDate" => $post->ToDate,
                     "AddedBy" => $post->AddedBy,
                     "UpdatedBy" => $post->UpdatedBy,
                     "Created_at" => $post->created_at,
@@ -79,90 +64,35 @@ class QueryMasterController extends Controller
 
     public function store(Request $request)
     {
-       // try{
+        try{
             call_logger('REQUEST COMES FROM QUERY ADD/UPDATE: '.$request->getContent());
 
             $id = $request->input('id');
             call_logger('new id:'.$id.'++');
             if($id==''){
-
-                $valuedata = array();
-                $valuedata = array(
-                    'Flight' => $request['ValueAddedServices']['Flight'],
-                    'Visa' => $request['ValueAddedServices']['Visa'],
-                    'Insurance' => $request['ValueAddedServices']['Insurance'],
-                    'Train' => $request['ValueAddedServices']['Train'],
-                    'Transfer' => $request['ValueAddedServices']['Transfer'],
-                );
-
-                $traveldata = array();
-                $traveldata = array(
-                    'Type' => $request['TravelDate']['Type'],
-                    'FromDate' => $request['TravelDate']['FromDate'],
-                    'ToDate' => $request['TravelDate']['ToDate'],
-                    'TotalDays' => $request['TravelDate']['TotalDays'],
-                    'SeasonType' => $request['TravelDate']['SeasonType'],
-                    'SeasonYear' => $request['TravelDate']['SeasonYear'],
-                );
-
-                $paxdata = array();
-                $paxdata = array(
-                    'Adult' => $request['PaxInfo']['Adult'],
-                    'Child' => $request['PaxInfo']['Child'],
-                    'Infant' => $request['PaxInfo']['Infant'],
-                );
-
-                $roomdata = array();
-                $roomdata = array(
-                    'Single' => $request['RoomInfo']['Single'],
-                    'Double' => $request['RoomInfo']['Double'],
-                    'Twin' => $request['RoomInfo']['Twin'],
-                    'Triple' => $request['RoomInfo']['Triple'],
-                    'ExtraBed' => $request['RoomInfo']['ExtraBed'],
-                );
                 $otp = mt_rand(100000, 999999);
 
                 call_logger('REQUEST COMES FROM : '.$request->Subject.'+++++++++++'.$request->ClientType);
 
-                
-
-                
-
-
                 $savedata = QueryMaster::create([
                     "QueryId" => $otp,
-                    "FDCode" => $request->FDCode,
-                    "PackageCode" => $request->PackageCode,
-                    "PackageName" => $request->PackageName,
                     "ClientType" => $request->ClientType,
-                    "AgentId" => $request->AgentId,
                     "LeadPax" => $request->LeadPax,
                     "Subject" => $request->Subject,
-                    "AddEmail" => $request->AddEmail,
-                    "AdditionalInfo" => $request->AdditionalInfo,
                     "QueryType" => $request->QueryType,
-                    "ValueAddedServices" => $valuedata,
-                    "TravelInfo" => $request->TravelInfo,
-                    "PaxType" => $request->PaxType,
-                    "TravelDate" => $traveldata,
-                    "PaxInfo" => $paxdata,
-                    "RoomInfo" => $roomdata,
                     "Priority" => $request->Priority,
                     "TAT" => $request->TAT,
-                    "TourType" => $request->TourType,
                     "LeadSource" => $request->LeadSource,
-                    "LeadRefrenceId" => $request->LeadRefrenceId,
-                    "HotelPrefrence" => $request->HotelPrefrence,
-                    "HotelType" => $request->HotelType,
-                    "MealPlan" => $request->MealPlan,
+                    "FromDate" => $request->TravelDate['FromDate'],
+                    "ToDate" => $request->TravelDate['ToDate'],
+                    "QueryJson" => $request->getContent(),
                     "AddedBy" => $request->AddedBy,
-                    "UpdatedBy" => $request->UpdatedBy,
                     'created_at' => now()
                 ]);
                 if ($savedata) {
-                    return response()->json(['Status' => 0, 'Message' => 'Data added successfully!']);
+                    return response()->json(['Status' => 1, 'Message' => 'Data added successfully!']);
                 } else {
-                    return response()->json(['Status' => 1, 'Message' =>'Failed to add data.'], 500);
+                    return response()->json(['Status' => 0, 'Message' =>'Failed to add data.'], 500);
                 }
             }else{
                 $edit = QueryMaster::find($id);
@@ -182,14 +112,14 @@ class QueryMasterController extends Controller
                     $edit->updated_at = now();
                     $edit->save();
 
-                    return response()->json(['Status' => 0, 'Message' => 'Data updated successfully']);
+                    return response()->json(['Status' => 1, 'Message' => 'Data updated successfully']);
                 }else{
-                    return response()->json(['Status' => 1, 'Message' => 'Failed to update data. Record not found.'], 404);
+                    return response()->json(['Status' => 0, 'Message' => 'Failed to update data. Record not found.'], 404);
                 }
             }
-        // }catch (\Exception $e){
-        //     call_logger("Exception Error  ===>  ". $e->getMessage());
-        //     return response()->json(['Status' => -1, 'Message' => 'Exception Error Found']);
-        // }
+        }catch (\Exception $e){
+            call_logger("Exception Error  ===>  ". $e->getMessage());
+            return response()->json(['Status' => -1, 'Message' => 'Exception Error Found']);
+        }
     }
 }
--- Code/Querybuilder/app/Models/QueryBuilder/QueryMaster.php
+++ Code/Querybuilder/app/Models/QueryBuilder/QueryMaster.php
@@ -11,42 +11,8 @@ class QueryMaster extends Model
     protected $table = _QUERY_MASTER_;
     protected $primarykey = 'id';
     protected $fillable = [
-          'QueryId',
-          'FDCode',
-          'PackageCode',
-          'PackageName',
-          'ClientType',
-          'AgentId',
-          'LeadPax', 
-          'Subject',
-          'AddEmail',
-          'AdditionalInfo',
-          "QueryType",
-          'ValueAddedServices',
-          'TravelInfo',
-          'PaxType',
-          'TravelDate',
-          'PaxInfo',
-          'RoomInfo', 
-          'Priority', 
-          'TAT', 
-          'TourType',
-          'LeadSource', 
-          'LeadRefrenceId',
-          'HotelPrefrence',
-          'HotelType',
-          'MealPlan',
-          'AddedBy', 
-          'UpdatedBy', 
-          //'QueryJson', 
-          'created_at', 
-          'updated_at'
+        'QueryId', 'ClientType', 'LeadPax', 'Subject', 'QueryType', 'Priority', 'TAT', 'LeadSource', 'AddedBy', 'UpdatedBy', 'FromDate', 'ToDate', 'QueryJson', 'created_at', 'updated_at'
     ];
 
-    protected $casts = [
-        'ValueAddedServices' => 'array',
-         'TravelDate' => 'array',
-        'PaxInfo' => 'array',
-        'RoomInfo' => 'array'
-    ];
+    public $timestamps = false;
 }
