9df9d9271564d3fd0df93d2bb5843a9b9bc92e25
Neelakshibaghe <neelakshi.baghel@deboxglobal.com>
2024-03-26 11:30:03 +0530

    AuthUserUpdate

--- Code/Services/Others/app/Http/Controllers/Master/AuthoriseUserController.php
+++ Code/Services/Others/app/Http/Controllers/Master/AuthoriseUserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use App\Models\Master\CreateUpdateUser;
+use App\Models\Master\CreateUpdateCompany;
 
 class AuthoriseUserController extends Controller
 {
@@ -20,9 +21,12 @@ class AuthoriseUserController extends Controller
         ->where('Email', $userId)
         ->where('Password', $password)
         ->get();
-        if ($posts->count() > 0) {
-            $response = Http::post('https://travcrm.in/Stratos/api/authoriseUser.php', [
-                'COMPANYKEY' => $posts[0]->CompanyKey,
+        if ($posts) {
+            $company = CreateUpdateCompany::select('LICENSEKEY')->where('id',$posts[0]->CompanyKey) // Add the column you want to fetch
+            ->get();
+           $response = Http::post('https://travcrm.in/Stratos/api/authoriseUser.php', [
+             
+                'COMPANYKEY' => $company[0]->LICENSEKEY,
                 'USERKEY' => $posts[0]->UserKey,
                 'ACTION' => $action
             ]);
@@ -43,8 +47,8 @@ class AuthoriseUserController extends Controller
 
              }  
         } else {
-            
-{}            return response()->json([
+                   
+     return response()->json([
                 'Status' => "-1",
                 'Message' => "User not Matched"
             ]);
