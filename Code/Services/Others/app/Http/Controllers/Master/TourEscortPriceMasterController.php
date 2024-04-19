3644cd1105e0823a83b8bcd86b971ece677ddb1d
Neelakshibaghe <neelakshi.baghel@deboxglobal.com>
2024-04-19 16:59:24 +0530

    Update TourEscortPriceMasterController.php

--- Code/Services/Others/app/Http/Controllers/Master/TourEscortPriceMasterController.php
+++ Code/Services/Others/app/Http/Controllers/Master/TourEscortPriceMasterController.php
@@ -71,7 +71,7 @@ class TourEscortPriceMasterController extends Controller
             if($id == '') {
 
                 $businessvalidation =array(
-                    'ServiceType' => 'required|unique:'._DB_.'.'._TOUR_ESCORT_PRICE_MASTER_.',ServiceType',
+                    'ServiceType' => 'required:'._DB_.'.'._TOUR_ESCORT_PRICE_MASTER_.',ServiceType',
                 );
 
                 $validatordata = validator::make($request->all(), $businessvalidation);
